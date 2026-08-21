217de7c31e8518f181a845f5e0496df460abd6b2
DarthTrooper10 <bjones39@harding.edu>
2026-08-21 10:19:22 -0500

    Floor 0 changes

--- project/floor-00-starter/main.cpp
+++ project/floor-00-starter/main.cpp
@@ -6,14 +6,15 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
-    std::cout << "\n=== THE ANTECHAMBER ===\n\n";
-    std::cout << "A torch sputters. The air smells of iron.\n";
-    std::cout << "What is your name, adventurer? ";
+    cout << "\n=== THE ANTECHAMBER ===\n\n";
+    cout << "A torch sputters. The air smells of iron.\n";
+    cout << "What is your name, adventurer? ";
 
-    std::string hero_name;
-    std::getline(std::cin, hero_name);
+    string hero_name;
+    getline(cin, hero_name);
 
     if (hero_name.empty()) {
         hero_name = "Nameless One";
@@ -21,14 +22,14 @@ int main() {
 
     // TODO Floor 0: replace this string with your own flavor line.
     // Anything a brave (or doomed) adventurer might mutter on descending.
-    const std::string flavor = "Change me, adventurer.";
+    const string flavor = "You will face many challenges adventurer. Be prepared or you will perish ";
 
-    std::cout << "\nWelcome, " << hero_name << ".\n";
-    std::cout << flavor << "\n\n";
-    std::cout << "The stair to Floor 1 awaits. Press Enter to rest.\n";
+    cout << "\nWelcome, " << hero_name << ".\n";
+    cout << flavor << "\n\n";
+    cout << "The stair to Floor 1 awaits. Press Enter to rest.\n";
 
-    std::string _;
-    std::getline(std::cin, _);
+    string _;
+    getline(cin, _);
 
     return 0;
 }
